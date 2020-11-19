ea9a1e5b9dde134a971d8780aaecf76af9e23232
Florent Hugouvieux <florent.hugouvieux@protonmail.com>
2020-11-19 22:17:06 +0100

    Add outro to PDF

--- tools/generate-use-cases-report.sh
+++ tools/generate-use-cases-report.sh
@@ -8,8 +8,9 @@ then
 fi
 cp -R ./assets/. ./tmp/
 find ./specifications/usecases/ -type f \( ! -iname "*md" -and ! -iname "*puml" \) -exec cp {} ./tmp/ \;
-find ./specifications/usecases/ -type f \( -iname "*md" \) -exec sed "s/[0-9].\([0-9].\?\)* //g" {} >> ./tmp/reports-fragments/use-cases.md \;
+find ./specifications/usecases/ -type f \( -iname "*md" \) -exec sed "s/[0-9].\([0-9].\?\)* //g" {} >> ./tmp/reports-fragments/use-cases-intro.md \;
 cd ./tmp
-pandoc ./reports-fragments/use-cases.md -f markdown+yaml_metadata_block --template eisvogel --table-of-contents --toc-depth 6 --number-sections --top-level-division=chapter --highlight-style breezedark -o ../output/report.pdf
+cat ./reports-fragments/use-cases-outro.md >> ./reports-fragments/use-cases-intro.md
+pandoc ./reports-fragments/use-cases-intro.md -f markdown+yaml_metadata_block --template eisvogel --table-of-contents --toc-depth 6 --number-sections --top-level-division=chapter --highlight-style breezedark -o ../output/report.pdf
 cd ..
 rm -rf ./tmp
