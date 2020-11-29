89f7f63391b15b7d68112c174594afee46a9e8f3
KevenDvorianoff <keven.dvorianoff@etu.umontpellier.fr>
2020-11-29 18:10:26 +0100

    using the hosted database

--- src/main/java/com/github/brokenswing/comixaire/dao/postgreSQL/ConnectionPostgreSQL.java
+++ src/main/java/com/github/brokenswing/comixaire/dao/postgreSQL/ConnectionPostgreSQL.java
@@ -1,8 +1,12 @@
 package com.github.brokenswing.comixaire.dao.postgreSQL;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Objects;
+import java.util.Properties;
 
 public class ConnectionPostgreSQL {
 
@@ -13,15 +17,32 @@ public class ConnectionPostgreSQL {
 
     public static Connection getConnection() {
         if (connection == null) {
+            Properties p = new Properties();
+            try
+            {
+                p.load(new InputStreamReader(
+                        Objects.requireNonNull(
+                                ConnectionPostgreSQL.class.getClassLoader()
+                                        .getResourceAsStream("database.properties"))
+                ));
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
             try {
-                String url = "";
-                String user = "";
-                String password = "";
+                String url = p.getProperty("url");
+                String user = p.getProperty("user");
+                String password = p.getProperty("password");
                 connection = DriverManager.getConnection(url, user, password);
             } catch (SQLException e) {
                 e.printStackTrace();
             }
         }
         return connection;
+    }
+
+    public static void main(String[] args) {
+        Connection connection = ConnectionPostgreSQL.getConnection();
     }
 }
