1bec084fb727f51c66853487ec5b2afabe08bd4b
Axel <axel.duval74@gmail.com>
2020-12-03 08:31:58 +0100

    Create client model

--- src/main/java/com/github/brokenswing/comixaire/models/Client.java
+++ src/main/java/com/github/brokenswing/comixaire/models/Client.java
@@ -2,5 +2,65 @@ package com.github.brokenswing.comixaire.models;
 
 public class Client
 {
-    //TODO: Fill
+    private int idClient;
+    private String firstname;
+    private String lastname;
+    private String gender;
+    private String address;
+    private Date birthdate;
+
+    public Client(String firstname, String lastname, String gender, String address, Date birthdate) {
+        this(-1, firstname, lastname, gender, address, birthdate);
+    }
+
+    public Client(int idClient, String firstname, String lastname, String gender, String address, Date birthdate) {
+        this.idClient = idClient;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.gender = gender;
+        this.address = address;
+        this.birthdate = birthdate;
+    }
+
+    public int getIdClient() {
+        return idClient;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+
 }
