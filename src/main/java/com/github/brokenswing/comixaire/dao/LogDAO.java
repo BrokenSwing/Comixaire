2434dd0856d25b10d65a2a6ac2e00e9af16679f0
Axel <axel.duval74@gmail.com>
2020-12-15 10:36:19 +0100

    Create LogDAO interface

---
+++ src/main/java/com/github/brokenswing/comixaire/dao/LogDAO.java
@@ -0,0 +1,15 @@
+package com.github.brokenswing.comixaire.dao;
+
+import com.github.brokenswing.comixaire.exception.InternalException;
+import com.github.brokenswing.comixaire.models.Log;
+
+/**
+ * Data access object to the logs. This interface allows
+ * to manipulate logs without being aware of the underlying
+ * system that stores the data.
+ */
+public interface LogDAO
+{
+    Log create(Log log) throws InternalException;
+    Log[] getAll() throws InternalException;
+}
