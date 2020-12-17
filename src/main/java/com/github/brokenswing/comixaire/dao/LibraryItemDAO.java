7a02c2db85c755007faafb30afee6690f1f65584
Axel <axel.duval74@gmail.com>
2020-12-17 11:59:33 +0100

    Create LibraryItem DAO

---
+++ src/main/java/com/github/brokenswing/comixaire/dao/LibraryItemDAO.java
@@ -0,0 +1,23 @@
+package com.github.brokenswing.comixaire.dao;
+
+import com.github.brokenswing.comixaire.exception.InternalException;
+import com.github.brokenswing.comixaire.exception.NoLibraryItemFoundException;
+import com.github.brokenswing.comixaire.models.Client;
+import com.github.brokenswing.comixaire.models.LibraryItem;
+
+public interface LibraryItemDAO
+{
+    LibraryItem create(LibraryItem libraryItem) throws InternalException;
+
+    LibraryItem[] findAll() throws InternalException;
+
+    LibraryItem findById(int libraryItem) throws InternalException, NoLibraryItemFoundException;
+
+    LibraryItem[] findByTitle(String title) throws InternalException, NoLibraryItemFoundException;
+
+    void update(LibraryItem libraryItem) throws InternalException;
+
+    void addBooking(LibraryItem libraryItem, Client client);
+
+    void delete(LibraryItem libraryItem) throws InternalException;
+}
