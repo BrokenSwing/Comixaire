0ab4828e20fde28e76ed95830e45c05e8a1b1557
Florent HUGOUVIEUX <florent.hugouvieux@protonmail.com>
2020-12-26 00:18:30 +0100

    Filter logs based on selected button

--- src/main/java/com/github/brokenswing/comixaire/controller/LogsController.java
+++ src/main/java/com/github/brokenswing/comixaire/controller/LogsController.java
@@ -3,38 +3,29 @@ package com.github.brokenswing.comixaire.controller;
 import com.github.brokenswing.comixaire.di.InjectValue;
 import com.github.brokenswing.comixaire.exception.InternalException;
 import com.github.brokenswing.comixaire.facades.logs.LogsFacade;
+import com.github.brokenswing.comixaire.javafx.NoOpSelectionModel;
 import com.github.brokenswing.comixaire.models.Log;
 import com.github.brokenswing.comixaire.view.LogCellView;
 import com.github.brokenswing.comixaire.view.ViewLoader;
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
-import javafx.scene.control.Button;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
-import javafx.scene.control.MultipleSelectionModel;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.temporal.ChronoField;
 import java.util.ResourceBundle;
-import java.util.stream.Stream;
 
 public class LogsController implements Initializable
 {
 
     private FilteredList<Log> filteredList;
-    private Stream<Log> allLogs;
 
-    @FXML
-    private Button todayButton;
-    @FXML
-    private Button weekButton;
-    @FXML
-    private Button monthButton;
-    @FXML
-    private Button allButton;
     @FXML
     private ListView<Log> logsList;
 
@@ -44,9 +35,42 @@ public class LogsController implements Initializable
     @InjectValue
     private ViewLoader loader;
 
-    private void displayLogs()
+    public void filterToday()
     {
-        logsList.setItems(this.filteredList);
+        LocalDate now = LocalDate.now();
+        this.filteredList.setPredicate(l ->
+        {
+            LocalDate logDate = l.getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+            return now.get(ChronoField.DAY_OF_YEAR) == logDate.get(ChronoField.DAY_OF_YEAR) &&
+                    now.get(ChronoField.YEAR) == logDate.get(ChronoField.YEAR);
+        });
+    }
+
+    public void filterWeek()
+    {
+        LocalDate now = LocalDate.now();
+        this.filteredList.setPredicate(l ->
+        {
+            LocalDate logDate = l.getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+            return now.get(ChronoField.ALIGNED_WEEK_OF_YEAR) == logDate.get(ChronoField.ALIGNED_WEEK_OF_YEAR) &&
+                    now.get(ChronoField.YEAR) == logDate.get(ChronoField.YEAR);
+        });
+    }
+
+    public void filterMonth()
+    {
+        LocalDate now = LocalDate.now();
+        this.filteredList.setPredicate(l ->
+        {
+            LocalDate logDate = l.getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+            return now.get(ChronoField.MONTH_OF_YEAR) == logDate.get(ChronoField.MONTH_OF_YEAR) &&
+                    now.get(ChronoField.YEAR) == logDate.get(ChronoField.YEAR);
+        });
+    }
+
+    public void filterAll()
+    {
+        this.filteredList.setPredicate(null);
     }
 
     @Override
@@ -60,9 +84,10 @@ public class LogsController implements Initializable
         {
             this.filteredList = new FilteredList<>(FXCollections.observableArrayList());
         }
-        this.logsList.setSelectionModel(new UnselectableModel());
+        this.logsList.setSelectionModel(new NoOpSelectionModel<>());
         this.logsList.setCellFactory(lv -> new LogListCell());
-        displayLogs();
+        this.logsList.setItems(this.filteredList);
+        this.filterToday();
     }
 
     private class LogListCell extends ListCell<Log>
@@ -81,90 +106,6 @@ public class LogsController implements Initializable
                 Node node = loader.loadView(new LogCellView(), item);
                 setGraphic(node);
             }
-        }
-    }
-
-    private class UnselectableModel extends MultipleSelectionModel<Log>
-    {
-        @Override
-        public ObservableList<Integer> getSelectedIndices()
-        {
-            return FXCollections.observableArrayList();
-        }
-
-        @Override
-        public ObservableList<Log> getSelectedItems()
-        {
-            return FXCollections.observableArrayList();
-        }
-
-        @Override
-        public void selectIndices(int index, int... indices)
-        {
-        }
-
-        @Override
-        public void selectAll()
-        {
-        }
-
-        @Override
-        public void selectFirst()
-        {
-        }
-
-        @Override
-        public void selectLast()
-        {
-        }
-
-        @Override
-        public void clearAndSelect(int index)
-        {
-        }
-
-        @Override
-        public void select(int index)
-        {
-        }
-
-        @Override
-        public void select(Log obj)
-        {
-        }
-
-        @Override
-        public void clearSelection(int index)
-        {
-        }
-
-        @Override
-        public void clearSelection()
-        {
-        }
-
-        @Override
-        public boolean isSelected(int index)
-        {
-            return false;
-        }
-
-        @Override
-        public boolean isEmpty()
-        {
-            return true;
-        }
-
-        @Override
-        public void selectPrevious()
-        {
-
-        }
-
-        @Override
-        public void selectNext()
-        {
-
         }
     }
 
---
+++ src/main/java/com/github/brokenswing/comixaire/javafx/NoOpSelectionModel.java
@@ -0,0 +1,91 @@
+package com.github.brokenswing.comixaire.javafx;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.MultipleSelectionModel;
+
+public class NoOpSelectionModel<T> extends MultipleSelectionModel<T>
+{
+
+    @Override
+    public ObservableList<Integer> getSelectedIndices()
+    {
+        return FXCollections.observableArrayList();
+    }
+
+    @Override
+    public ObservableList<T> getSelectedItems()
+    {
+        return FXCollections.observableArrayList();
+    }
+
+    @Override
+    public void selectIndices(int index, int... indices)
+    {
+    }
+
+    @Override
+    public void selectAll()
+    {
+    }
+
+    @Override
+    public void selectFirst()
+    {
+    }
+
+    @Override
+    public void selectLast()
+    {
+    }
+
+    @Override
+    public void clearAndSelect(int index)
+    {
+    }
+
+    @Override
+    public void select(int index)
+    {
+    }
+
+    @Override
+    public void select(T obj)
+    {
+    }
+
+    @Override
+    public void clearSelection(int index)
+    {
+    }
+
+    @Override
+    public void clearSelection()
+    {
+    }
+
+    @Override
+    public boolean isSelected(int index)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean isEmpty()
+    {
+        return true;
+    }
+
+    @Override
+    public void selectPrevious()
+    {
+
+    }
+
+    @Override
+    public void selectNext()
+    {
+
+    }
+
+}
