0ae257c6dabe2845309ffc8c5f36c6ad12d91481
Axel <axel.duval74@gmail.com>
2020-12-31 16:33:39 +0100

    Create SubscriptionDAO

--- src/main/java/com/github/brokenswing/comixaire/dao/postgres/PostgresSubscriptionsDAO.java
+++ src/main/java/com/github/brokenswing/comixaire/dao/postgres/PostgresSubscriptionsDAO.java
@@ -1,2 +1,31 @@
-package com.github.brokenswing.comixaire.dao.postgres;public class PostgresSubscriptionsFactory {
+package com.github.brokenswing.comixaire.dao.postgres;
+
+import com.github.brokenswing.comixaire.dao.SubscriptionsDAO;
+import com.github.brokenswing.comixaire.exception.InternalException;
+import com.github.brokenswing.comixaire.exception.NoClientFoundException;
+import com.github.brokenswing.comixaire.models.Subscription;
+
+import java.sql.Connection;
+
+public class PostgresSubscriptionsDAO implements SubscriptionsDAO
+{
+    private Connection connection;
+    public PostgresSubscriptionsDAO(Connection connection)
+    {
+        this.connection = connection;
+    }
+
+    @Override
+    public Subscription create(Subscription subscription) throws InternalException
+    {
+        //TODO: !!PROBLEME DE SPECIFICATION!!
+        return null;
+    }
+
+    @Override
+    public Subscription[] findAllByCardId(String idCard) throws InternalException, NoClientFoundException
+    {
+        //TODO: !!PROBLEME DE SPECIFICATION!!
+        return new Subscription[0];
+    }
 }
