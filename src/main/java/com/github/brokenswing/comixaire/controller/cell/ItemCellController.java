8005b660c885c4fb6b508369f88d000e597cdbd5
Axel <axel.duval74@gmail.com>
2021-01-02 11:50:00 +0100

    Add confirmation before removing item

--- src/main/java/com/github/brokenswing/comixaire/controller/cell/ItemCellController.java
+++ src/main/java/com/github/brokenswing/comixaire/controller/cell/ItemCellController.java
@@ -6,15 +6,18 @@ import com.github.brokenswing.comixaire.exception.InternalException;
 import com.github.brokenswing.comixaire.facades.item.LibraryItemFacade;
 import com.github.brokenswing.comixaire.models.*;
 import com.github.brokenswing.comixaire.view.ClientDetailsView;
+import com.github.brokenswing.comixaire.view.ClientsView;
 import com.github.brokenswing.comixaire.view.ItemsView;
 import com.github.brokenswing.comixaire.view.alert.InternalErrorAlert;
 import com.github.brokenswing.comixaire.view.util.Router;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.text.Text;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class ItemCellController implements ParametrizedController<LibraryItem>, Initializable
@@ -44,21 +47,27 @@ public class ItemCellController implements ParametrizedController<LibraryItem>, 
     @FXML
     protected void delete()
     {
-        //TODO: create alert to confirm the deletion
-        try
-        {
-            this.itemsFacade.delete(libraryItem);
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setTitle("Success");
-            alert.setContentText("The library item \"" + libraryItem.getTitle() + "\" was successfully deleted.");
-            alert.showAndWait();
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Are you sure ?");
+        alert.setHeaderText("Do you really want to delete: " + libraryItem.getTitle() + " ?");
+        Optional<ButtonType> result = alert.showAndWait();
+        if(result.isPresent() && result.get() == ButtonType.OK){
+            try
+            {
+                this.itemsFacade.delete(libraryItem);
+                Alert alert2 = new Alert(Alert.AlertType.INFORMATION);
+                alert2.setTitle("Success");
+                alert2.setContentText("The library item \"" + libraryItem.getTitle() + "\" was successfully deleted.");
+                alert2.showAndWait();
+                router.navigateTo(new ItemsView());
+            }
+            catch (InternalException e)
+            {
+                e.printStackTrace();
+                new InternalErrorAlert(e).showAndWait();
+            }
         }
-        catch (InternalException e)
-        {
-            e.printStackTrace();
-            new InternalErrorAlert(e).showAndWait();
-        }
-        router.navigateTo(new ItemsView());
+
     }
 
     @Override
