2bb97e978d4f97e7209051324ba6aaef3b487495
KevenDvorianoff <keven.dvorianoff@etu.umontpellier.fr>
2021-01-02 22:51:11 +0100

    Implement getAllRating method

--- src/main/java/com/github/brokenswing/comixaire/dao/postgres/PostgresRatingDAO.java
+++ src/main/java/com/github/brokenswing/comixaire/dao/postgres/PostgresRatingDAO.java
@@ -2,14 +2,16 @@ package com.github.brokenswing.comixaire.dao.postgres;
 
 import com.github.brokenswing.comixaire.dao.RatingDAO;
 import com.github.brokenswing.comixaire.exception.InternalException;
+import com.github.brokenswing.comixaire.models.Client;
 import com.github.brokenswing.comixaire.models.LibraryItem;
 import com.github.brokenswing.comixaire.models.Rating;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class PostgresRatingDAO implements RatingDAO
 {
-    private Connection connection;
+    private final Connection connection;
 
     public PostgresRatingDAO(Connection connection) { this.connection = connection; }
 
@@ -30,8 +32,33 @@ public class PostgresRatingDAO implements RatingDAO
     @Override
     public Rating[] getAllRating() throws InternalException
     {
-        //TODO: implement
-        return new Rating[0];
+        try
+        {
+            ResultSet result = connection.prepareStatement(
+                    "SELECT * FROM rating " +
+                    "NATURAL JOIN clients " +
+                    "NATURAL JOIN libraryitems " +
+                    "NATURAL LEFT JOIN books " +
+                    "NATURAL LEFT JOIN cd " +
+                    "NATURAL LEFT JOIN dvd " +
+                    "NATURAL LEFT JOIN games").executeQuery();
+
+            ArrayList<Rating> ratings = new ArrayList<>();
+
+            while(result.next()){
+                LibraryItem item = PostgresLibraryItemDAO.libraryItemFromRow(result);
+                Client client = PostgresClientDAO.clientFromRow(result);
+                if (item != null) {
+                    ratings.add(new Rating(client, item, result.getInt("note")));
+                }
+            }
+
+            return ratings.toArray(new Rating[0]);
+        }
+        catch (SQLException e)
+        {
+            throw new InternalException("Unable to find any rating", e);
+        }
     }
 
 }
