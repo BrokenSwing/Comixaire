dbe34964159cb3048233a5f104ed26438af5048d
Axel <axel.duval74@gmail.com>
2021-01-02 11:40:07 +0100

    Start recommendation feature

---
+++ src/main/java/com/github/brokenswing/comixaire/controller/ClientRecommendationsController.java
@@ -0,0 +1,37 @@
+package com.github.brokenswing.comixaire.controller;
+
+import com.github.brokenswing.comixaire.controller.util.ParametrizedController;
+import com.github.brokenswing.comixaire.di.InjectValue;
+import com.github.brokenswing.comixaire.models.Client;
+import com.github.brokenswing.comixaire.view.ClientActionCenterView;
+import com.github.brokenswing.comixaire.view.util.Router;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClientRecommendationsController implements ParametrizedController<Client>, Initializable
+{
+    private Client client;
+
+    @InjectValue
+    private Router router;
+    @Override
+    public void handleViewParam(Client client)
+    {
+        this.client = client;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources)
+    {
+        //TODO: implement
+    }
+
+    public void back() { router.navigateTo(new ClientActionCenterView()); }
+
+    public void search()
+    {
+        //TODO: filter recommendations
+    }
+}
