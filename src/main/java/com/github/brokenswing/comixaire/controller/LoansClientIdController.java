c54c88924c5b85eb5a5180636c05143452e891c4
Axel <axel.duval74@gmail.com>
2021-01-05 09:45:37 +0100

    Add boolean binding to Button

--- src/main/java/com/github/brokenswing/comixaire/controller/LoansClientIdController.java
+++ src/main/java/com/github/brokenswing/comixaire/controller/LoansClientIdController.java
@@ -6,16 +6,27 @@ import com.github.brokenswing.comixaire.exception.InternalException;
 import com.github.brokenswing.comixaire.exception.NoClientFoundException;
 import com.github.brokenswing.comixaire.facades.clients.ClientsFacade;
 import com.github.brokenswing.comixaire.models.Client;
+import com.github.brokenswing.comixaire.utils.FormValidationBuilder;
 import com.github.brokenswing.comixaire.view.Views;
 import com.github.brokenswing.comixaire.view.util.Router;
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.BooleanBinding;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
-public class LoansClientIdController
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.ResourceBundle;
+
+public class LoansClientIdController implements Initializable
 {
     @FXML
     public TextField loanClientIdField;
+    @FXML
+    public Button loanClientIdButton;
 
     @InjectValue
     private ClientsFacade clientsFacade;
@@ -45,5 +56,14 @@ public class LoansClientIdController
             alert.setHeaderText("The client with this card is not in our database");
             alert.showAndWait();
         }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources)
+    {
+        BooleanBinding isValid = new FormValidationBuilder()
+                .notEmpty(loanClientIdField.textProperty())
+                .build();
+        loanClientIdButton.disableProperty().bind(isValid.not());
     }
 }
