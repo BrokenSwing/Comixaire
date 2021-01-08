21df1ed0c4abfdfc690dd123fd283f3669801af7
KevenDvorianoff <keven.dvorianoff@etu.umontpellier.fr>
2021-01-08 15:40:28 +0100

    Add finetype in bdd

---
+++ migrations/005.add-fineType.sql
@@ -0,0 +1,8 @@
+INSERT INTO finetype(finetype_label, finetype_price)
+VALUES('Damages level 1', 3),
+      ('Damages level 2', 5),
+      ('Damages level 3', 10),
+      ('Late < 1 week', 2),
+      ('Late < 2 weeks', 4),
+      ('Late > 2 weeks', 7),
+      ('None', 0);
